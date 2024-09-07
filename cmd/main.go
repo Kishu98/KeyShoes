ac87b1cde28fa377422dfc354ddddcd4a881785a
kishu98 <kishujain.1998@gmail.com>
2024-09-07 22:52:40 +0530

    adding gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	db "backend/internals"
-
-	"github.com/gin-gonic/gin"
+	db "blog-site/internals"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 
 	db.ConnectDB()
 
-	router := gin.Default()
+	http.HandleFunc("/", helloHandler)
 
-	router.Run(":8080")
+	log.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
